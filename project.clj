0a80aadd6faf671f6991a7fc1c8b2b11f4509cfb
Christopher T Small <metasoarous@gmail.com>
2017-04-10 18:31:26 -0700

    Intial commit

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject datsys-papers-scratchpad "0.0.1-SNAPSHOT"
+  :description "for hacking the datsync"
+  :url "https://github.com/metasoarous/datsys-papers-scratchpad"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.async "0.3.442"]
+                 [datascript "0.15.5"]
+                 [posh "0.5.5"]
+                 [reagent "0.6.0"]
+                 [re-com "0.8.3"]
+                 [com.taoensso/timbre "4.8.0"]]
+  :repl-options {:port 34398})
---
+++ src/scratch/differential_dataflow.clj
@@ -0,0 +1,3 @@
+(ns scratch.differential-dataflow)
+
+
